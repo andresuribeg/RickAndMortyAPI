e389af7e31a89fa9d9d6b74b4ccd35399b52fbb5
Andres Uribe <neluri@comfenalcoquindio.co>
2024-01-16 21:55:02 -0500

    base Url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RickService } from './rick.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'RickAndMortyAPI';
+  constructor(private rickyMortyService: RickService) { }
+
+  ngOnInit():void {
+    
+  }
 }
--- src/app/rick.service.ts
+++ src/app/rick.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RickService {
+  private baseUrl = 'https://rickandmortyapi.com/api/'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getcharacter () {
+    return this.http.get(this.baseUrl+'/character')
+  }
 }
